ae778544f205311493a4c7c7c1cece9cd44b4e34
carole tierney <caroleatierney@gmail.com>
2025-03-22 15:10:59 -0400

    changed vite build base again

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from "@tailwindcss/vite";
-
-// export default defineConfig({
-//   base: "/Portfolio/caroleatierney/",
-//   build: {
-//     outDir: "build",
-//   },
-//   plugins: [
-//     react(),
-//     tailwindcss()
-//   ],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "/",
   build: {
     outDir: "build",
   },
