a0e6751caebabb914cef82d2ea965e1108d5b5b4
carole tierney <caroleatierney@gmail.com>
2025-04-06 18:14:54 -0400

    still issues with url

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/",
-  build: {
-    outDir: "build",
-  },
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_BASE_URL || "/",
+    build: {
+      outDir: "build",
+    },
+    plugins: [react()],
+  };
 });
