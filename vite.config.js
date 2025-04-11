0d21b674adb4277524cf839d4d02ff34ed5f8081
carole tierney <caroleatierney@gmail.com>
2025-04-10 20:33:04 -0400

    renaming homepage to slash

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: env.VITE_BASE_URL || "/",
+    base: "/",
     build: {
       outDir: "build",
     },
