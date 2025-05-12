35f2d323e40579138a1293c8e1c4351b34c796c0
carole tierney <caroleatierney@gmail.com>
2025-05-12 11:40:09 -0400

    all projects added

--- src/Components/BootCamp.jsx
+++ src/Components/BootCamp.jsx
@@ -6,6 +6,8 @@ import Maggie from "./../Assets/MensSalon.jpeg";
 import Gene from "./../Assets/GeneShalit.png";
 import Soap from "./../Assets/SoapScents.png";
 import Marshfield from "./../Assets/MarBeachRec.png";
+import Planner from "./../Assets/DinnerPlanner.png";
+import TicTacToe from "./../Assets/TicTacToe.png";
 
 export default function BootCamp() {
   return (
@@ -285,7 +287,7 @@ export default function BootCamp() {
         <div className="bg-slate-600 border-8 border-slate-900 mt-10 mx-auto">
           <div className="text-slate-300 my-5">
             <h3 className="text-md md:text-4xl text-center text-yellow-700">
-              Men's Salon
+              Movie Review
             </h3>
             <p className="m-3 text-center text-xs md:text-lg p-2 md:w-3/4 mx-auto">
               The movie review app was an assignmnent to practice coding React
@@ -365,7 +367,7 @@ export default function BootCamp() {
         {/* ******************    Marshfield Travel App     ****************** */}
         <div className="bg-slate-600 border-8 border-slate-900 mt-10 mx-auto">
           <div className="text-slate-300 my-5">
-            <h3 className="text-md md:text-4xl text-center text-yellow-700">
+            <h3 className="text-md md:text-4xl text-center text-blue-400">
               Marshfield Travel App
             </h3>
             <p className="m-3 text-center text-xs md:text-lg p-2 md:w-3/4 mx-auto">
@@ -380,7 +382,7 @@ export default function BootCamp() {
               <div className="justify-center m-1 md:my-5 md:w-1/2 items-center">
                 <div className="w-full mx-auto">
                   <img
-                    className="w-full border-8 border-yellow-700 mx-auto h-full"
+                    className="w-full border-8 border-blue-400 mx-auto h-full"
                     src={Marshfield}
                     alt={"Marshfield"}
                   />
@@ -450,12 +452,91 @@ export default function BootCamp() {
             </div>
           </div>
         </div>
+       
+        {/* ******************    Tic-Tac-Toe     ****************** */}
+        <div className="bg-slate-600 border-8 border-slate-900 mt-10 mx-auto">
+          <div className="text-slate-300 my-5">
+            <h3 className="text-md md:text-4xl text-center text-teal-600">
+              Dinner Planner
+            </h3>
+            <p class="project-description">
+              This application was designed to practice DOM manipulation, forms
+              and tables. It uses Bootstrap and can add and delete rows from the
+              table. The data in the table is hard coded to populate.
+            </p>
+
+            {/* Main Div */}
+            <div className="flex flex-col md:flex-row justify-center md:m-5">
+              {/* Left side section of larger screen */}
+              <div className="justify-center m-1 md:my-5 md:w-1/2 items-center">
+                <div className="w-full mx-auto">
+                  <img
+                    className="w-full border-8 border-teal-600 mx-auto h-full"
+                    src={Planner}
+                    alt={"Dinner Planner"}
+                  />
+                </div>
+
+                <div className="flex flex-row justify-evenly text-center text-xs my-3">
+                  <a
+                    href="https://caroleatierney.github.io/Dinner_Planner/"
+                    class="website-link"
+                    title="View Live"
+                    target="_blank"
+                  >
+                    Live Website
+                  </a>
+                  <a
+                    href="https://github.com/caroleatierney/Dinner_Planner"
+                    class="website-link"
+                    title="View Code"
+                    target="_blank"
+                  >
+                    View Code
+                  </a>
+                </div>
+              </div>
+
+              {/* Right side section of larger screen */}
+
+              <div className="md:pl-10 flex flex-col md:w-1/2 items-center justify-center mx-auto text-md">
+                <div className="flex flex-col pt-5">
+                  {/* Horizontal list container */}
+                  <div className="pb-3">
+                    <h3 className="text-md md:text-lg lg:text-3xl">
+                      Notable features:
+                    </h3>
+                    <ul>
+                      <li>Bootstrap Table</li>
+                      <li>Bootstrap Form</li>
+                    </ul>
+                  </div>
+
+                  <div>
+                    <h3 className="text-md md:text-3xl">Technologies used:</h3>
+                    <ul className="text-xs list-disc list-inside">
+                      <li>DOM Manipulation</li>
+                      <li>CSS</li>
+                      <li>Bootstrap</li>
+                    </ul>
+                    <h3 className="text-md md:text-3xl">
+                      Room for improvement:
+                    </h3>
+                    <ul className="text-xs list-disc list-inside">
+                      <li>Add MockAPI to seed/save table data</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* ******************    Soap Recipe App     ****************** */}
         <div className="bg-slate-600 border-8 border-slate-900 mt-10 mx-auto">
           <div className="text-slate-300 my-5">
-            <h3 className="text-md md:text-4xl text-center text-orange-300">
-              Men's Salon
+            <h3 className="text-md md:text-4xl text-center text-green-600">
+              Soap Recipe
             </h3>
             <p className="m-3 text-center text-xs md:text-lg p-2 md:w-3/4 mx-auto">
               This applicationt allows users to use a soap recipe application to
@@ -468,7 +549,7 @@ export default function BootCamp() {
               <div className="justify-center m-1 md:my-5 md:w-1/2 items-center">
                 <div className="w-full mx-auto">
                   <img
-                    className="w-full border-8 border-amber-200 mx-auto h-full"
+                    className="w-full border-8 border-green-600 mx-auto h-full"
                     src={Soap}
                     alt={"Soap"}
                   />
@@ -539,7 +620,89 @@ export default function BootCamp() {
                       <li>Login</li>
                       <li>It's ugly!</li>
                     </ul>
-                    v
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* ******************    Tic-Tac-Toe     ****************** */}
+        <div className="bg-slate-600 border-8 border-slate-900 mt-10 mx-auto">
+          <div className="text-slate-300 my-5">
+            <h3 className="text-md md:text-4xl text-center text-white">
+              Tic-Tac-Toe
+            </h3>
+            <p class="project-description">
+              This application was to design a Tic-Tac-Toe game and display the
+              current player, winner and whether there was a tie. There is an
+              option to clear the grid and start over. I tried to add some fun
+              images to play our family's betta fishes against eachother.
+            </p>
+
+            {/* Main Div */}
+            <div className="flex flex-col md:flex-row justify-center md:m-5">
+              {/* Left side section of larger screen */}
+              <div className="justify-center m-1 md:my-5 md:w-1/2 items-center">
+                <div className="w-full mx-auto">
+                  <img
+                    className="w-full border-8 border-white mx-auto h-full"
+                    src={TicTacToe}
+                    alt={"Tic-Tac-Toe"}
+                  />
+                </div>
+
+                <div className="flex flex-row justify-evenly text-center text-xs my-3">
+                  <a
+                    href="https://caroleatierney.github.io/Tic_Tac_Toe"
+                    class="website-link"
+                    title="View Live"
+                    target="_blank"
+                  >
+                    Live Website
+                  </a>
+                  <a
+                    href="https://github.com/caroleatierney/Tic_Tac_Toe"
+                    class="website-link"
+                    title="View Code"
+                    target="_blank"
+                  >
+                    View Code
+                  </a>
+                </div>
+              </div>
+
+              {/* Right side section of larger screen */}
+
+              <div className="md:pl-10 flex flex-col md:w-1/2 items-center justify-center mx-auto text-md">
+                <div className="flex flex-col pt-5">
+                  {/* Horizontal list container */}
+                  <div className="pb-3">
+                    <h3 className="text-md md:text-lg lg:text-3xl">
+                      Notable features:
+                    </h3>
+                    <ul>
+                      <li>Although clunky, a lot of great DOM manipulation</li>
+                    </ul>
+                  </div>
+
+                  <div>
+                    <h3 className="text-md md:text-3xl">Technologies used:</h3>
+                    <ul className="text-xs list-disc list-inside">
+                      <li>CSS Grid</li>
+                      <li>DOM Manipulation</li>
+                    </ul>
+                    <h3 className="text-md md:text-3xl">
+                      Room for improvement:
+                    </h3>
+                    <ul className="text-xs list-disc list-inside">
+                      <li>Grid logic to determine winner - not bad loop</li>
+                      <li>fix clunky</li>
+                      <li>Put image in square with X or O over it</li>
+                      <li>Getting a 404?</li>
+                      <li>make responsive</li>
+                      <li>Correct fish name - not player 1 or 2</li>
+                    </ul>
                   </div>
                 </div>
               </div>
@@ -550,7 +713,7 @@ export default function BootCamp() {
         {/* ******************    South Shore Men's Salon     ****************** */}
         <div className="bg-slate-600 border-8 border-slate-900 mt-10 mx-auto">
           <div className="text-slate-300 my-5">
-            <h3 className="text-md md:text-4xl text-center text-orange-300">
+            <h3 className="text-md md:text-4xl text-center text-amber-200">
               Men's Salon
             </h3>
             <p className="m-3 text-center text-xs md:text-lg p-2 md:w-3/4 mx-auto">
