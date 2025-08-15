cf51b80aab7933e2a975c94727131ac2f5c9d926
carole tierney <caroleatierney@gmail.com>
2025-08-15 15:06:12 -0400

    Need to make a change before I forget how to do it

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,43 +1,57 @@
 import React from 'react';
+import HeadShot from "./../Assets/HeadShot.jpeg";
 
 export default function About() {
   return (
     <>
-      <div className="flex flex-col mx-auto text-center text-grey-900 w-3/4 pt-10">
-        <h3 className="flex justify-center text-md md:text-4xl">About</h3>
-        <p className="font-extralight text-sm md:text-lg p-3">
-          I am an aspiring web developer working on mostly personal projects. I
-          am no longer seeking a Front End Web Developer position, as I am back
-          on the AS400 - Green Screen and all! I have experience in full stack
-          web development using technologies such as HTML, CSS, JavaScript,
-          NodeJS, PHP, Bootstrap, React Bootstrap, Tailwind, Flowbite-React,
-          Bulma, Git, Heroku, MongoDB, PostgreSQL and jQuery. In my spare time,
-          I enjoy painting murals, making cold process soap and hanging out with
-          my mini Aussie, Nina. Please take a look at some of my websites! This
-          Website uses React, FlowBite React, Tailwind and Vite. It is hosted on
-          GitHub Pages.
-        </p>
-        <h3 className="text-red-500">
-          This Portfolio and all of the projects are a work in Progress.
+      <div className="flex flex-row">
+        <div className="flex flex-col mx-auto text-center justify-center text-grey-900 w-3/4 pt-10 px-5">
+          <h3 className="flex justify-center text-md md:text-4xl">About</h3>
+          <p className="font-extralight text-sm md:text-xl p-3">
+            I am an aspiring web developer working on mostly personal projects.
+            I am no longer seeking a Front End Web Developer position, as I am
+            back on the AS400 - Green Screen and all! I have experience in full
+            stack web development using technologies such as HTML, CSS,
+            JavaScript, NodeJS, PHP, Bootstrap, React Bootstrap, Tailwind,
+            Flowbite-React, Bulma, Git, Heroku, MongoDB, PostgreSQL and jQuery.
+            In my spare time, I enjoy painting murals, making cold process soap
+            and hanging out with my mini Aussie, Nina. Please take a look at
+            some of my websites! This Website uses React, FlowBite React,
+            Tailwind and Vite. It is hosted on GitHub Pages.
+          </p>
           <h3 className="text-red-500">
-            Since no one will ever look at this, I will keep my checklist here.
+            This Portfolio and all of the projects are a work in Progress.
+            <h3 className="text-red-500">
+              Since no one will ever look at this, I will keep my checklist
+              here.
+            </h3>
           </h3>
-        </h3>
-        <h3 className="text-red-500">Checklist to fix next:</h3>
-        <h3 className="text-red-500">
-          - Vertical spacing on right side of Projects
-        </h3>
-        <h3 className="text-red-500">
-          - Add Face in about
-        </h3>
+          <h3 className="text-red-500">Checklist to fix next:</h3>
+          <h3 className="text-red-500">
+            - Vertical spacing on right side of Projects
+          </h3>
 
-        <h3 className="text-red-500">- Make Responsive</h3>
-        <h3 className="text-red-500">- Add Asleigh's 15 minutes of fame to post - how can you make it impressive</h3>
-        <h3 className="text-red-500">- Add Mike's Field goal - Make impressive</h3>
-        <h3 className="text-red-500">
-          - Look at Before GA Repository: Artwork @
-          https://caroleatierney.github.io/Before-GA-Repository/ - Add an artwork carousel
-        </h3>
+          <h3 className="text-red-500">- Make Responsive</h3>
+          <h3 className="text-red-500">
+            - Add Asleigh's 15 minutes of fame to post - how can you make it
+            impressive
+          </h3>
+          <h3 className="text-red-500">
+            - Add Mike's Field goal - Make impressive
+          </h3>
+          <h3 className="text-red-500">
+            - Look at Before GA Repository: Artwork @
+            https://caroleatierney.github.io/Before-GA-Repository/ - Add an
+            artwork carousel
+          </h3>
+        </div>
+        <div className="flex flex-col mx-auto text-center text-grey-900 w-3/4 pt-10">
+          <img
+              className="w-2/3 border-12 border-gray-400 mx-auto h-full"
+              src={HeadShot}
+              alt={"HeadShot of Carole Tierney"}
+          /> 
+        </div>
       </div>
     </>
   );
