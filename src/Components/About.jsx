805cb4f2925ccca9b0a3a34f08d2b20993c3dd70
carole tierney <caroleatierney@gmail.com>
2026-05-08 13:19:53 -0400

    fix non y=tailwind code

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
+import React from "react";
 import HeadShot from "./../Assets/HeadShot.jpeg";
 
 export default function About() {
   return (
-    <>
-      <div className="flex flex-col md:flex-row items-center mx-auto">
-        <div className="w-full md:w-2/3 text-center text-gray-900 p-200">
-          <h3 className="text-xs md:text-8xl lg-text-9xl">About</h3>
+    <div className="flex flex-col md:flex-row items-center mx-auto">
+      <div className="w-full md:w-2/3 text-center text-gray-900 p-10">
+        <h3 className="text-xs md:text-8xl lg:text-9xl">About</h3>
 
-          <p className="font-extralight text-xs sm:text-sm md:text-7xl pt-10 lg-text-9xl leading-relaxed">
-            I am an aspiring web developer working on mostly personal projects.
-            I am no longer seeking a Front End Web Developer position, as I am
-            back on the AS400 - Green Screen and all! I have experience in full
-            stack web development using technologies such as HTML, CSS,
-            JavaScript, NodeJS, PHP, Bootstrap, React Bootstrap, Tailwind,
-            Flowbite-React, Bulma, Git, Heroku, MongoDB, PostgreSQL and jQuery.
-          </p>
+        <p className="font-extralight text-xs sm:text-sm md:text-7xl pt-10 leading-relaxed">
+          I am an aspiring web developer working on mostly personal projects. I
+          am no longer seeking a Front End Web Developer position, as I am back
+          on the AS400 - Green Screen and all! I have experience in full stack
+          web development using technologies such as HTML, CSS, JavaScript,
+          NodeJS, PHP, Bootstrap, React Bootstrap, Tailwind, Flowbite-React,
+          Bulma, Git, Heroku, MongoDB, PostgreSQL, and jQuery.
+        </p>
 
-          <p className="font-extralight text-xs sm:text-sm md:text-7xl pt-40 lg-text-9xl leading-relaxed">
-            In my spare time, I enjoy painting murals, making cold process soap
-            and hanging out with my mini Aussie, Nina. Please take a look at
-            some of my websites! This Website uses React, FlowBite React,
-            Tailwind and Vite. It is hosted on GitHub Pages.
-          </p>
-        </div>
-        <div className="w-full md:w-1/3 pt-6 md:pt-10 px-5">
-          <img
-            className="w-full border-40 border-gray-400 mx-auto h-auto rounded-lg"
-            src={HeadShot}
-            alt={"HeadShot of Carole Tierney"}
-          />
-        </div>
+        <p className="font-extralight text-xs sm:text-sm md:text-7xl pt-36 leading-relaxed">
+          In my spare time, I enjoy painting murals, making cold process soap,
+          and hanging out with my mini Aussie, Nina. Please take a look at some
+          of my websites! This Website uses React, Flowbite React, Tailwind, and
+          Vite. It is hosted on GitHub Pages.
+        </p>
       </div>
-    </>
+
+      <div className="w-full md:w-1/3 pt-6 md:pt-10 px-5">
+        <img
+          className="w-full h-auto border-4 border-gray-400 mx-auto rounded-lg"
+          src={HeadShot}
+          alt="HeadShot of Carole Tierney"
+        />
+      </div>
+    </div>
   );
 }
