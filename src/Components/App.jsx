b8b34a510c5abfc3b22fb0171b95a667362bf766
carole tierney <caroleatierney@gmail.com>
2026-05-29 14:04:26 -0400

    fix refresh again with HASH Router

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Header from "../Components/Header";
 // import Home from "../Components/Home";
 import About from "../Components/About";
